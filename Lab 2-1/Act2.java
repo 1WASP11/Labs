f53fd79f5039b3694b1ce0bfb4bdccfa0487feaa
1WASP11 <volodarskijoskar00@gmail.com>
2025-02-17 21:26:04 +0200

    Переміщення файлів до папки

--- Lab 2-1/Act2.java
+++ Lab 2-1/Act2.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+//Дано довжини ребер A, B, C прямокутного паралелепіпеда. Знайти його об’єм та площу поверхні.
+
 public class Act2 {   
     public static void main(String[] args) throws IOException
 {
